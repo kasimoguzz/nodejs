56bce35fdc5fbb42018a6df355c927b021a5ca8c
muhammed kasim oguz <muhammedkasimoguz@gmail.com>
2023-05-15 22:18:59 +0300

    modul | export

---
+++ odev3-circle/index.js
@@ -0,0 +1,8 @@
+
+
+// baska bir js dosyasindan modul seklinde fonksiyonlar alindi ve degiskene atandi
+let circle = require('./circle')
+
+// degisken uzerinden fonksiyonlar cagrildi
+circle.circleArea(5)
+circle.circleCircumference(5)
